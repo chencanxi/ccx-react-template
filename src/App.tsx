444ab5459be39abb053106ef5318be4802be8fcf
chencanxi <chencanxi@zhiketong.cn>
2025-01-05 21:48:33 +0800

    init ts

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>CCX React 18 + TS + Vite</h1>
+      <p>Hello, TypeScript!</p>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // 可根据需要自定义，比如 server.port 等
 });
